e34d34cd3f38126023156468bdb7b2925b11715e
Charles Benello <31220609+cmbenello@users.noreply.github.com>
2023-12-27 11:06:53 +0000

    about to make some radical changes

--- app.js
+++ app.js
@@ -27,3 +27,17 @@ window.addEventListener("load", () => {
         }
     });
 });
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('show');
+        }
+        else{
+            entry.target.classList.remove('show');
+        }
+    });
+})
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
