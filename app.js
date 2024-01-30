03a756c235c2223af937765cfde9316a6eeef1d4
Charles Benello <31220609+cmbenello@users.noreply.github.com>
2024-01-29 19:56:04 -0600

    Removed the Loader

--- app.js
+++ app.js
@@ -1,31 +1,36 @@
-// This part is for finding when the page is done loading
-window.addEventListener("load", () => {
-    const loader = document.querySelector(".loader");
-    const loaderText = loader.querySelector('.loader-text');
-    const loaderImage = loader.querySelector('img');
-    const fadetime = 500;
+// For some reason the laoder makes it too slow :(
+// // This part is for finding when the page is done loading
+// window.addEventListener("load", () => {
+//     const loader = document.querySelector(".loader");
+//     const loaderText = loader.querySelector('.loader-text');
+//     const loaderImage = loader.querySelector('img');
+//     const fadetime = 500;
 
-    // First, fade out the text
-    setTimeout(() => {
-        loaderText.style.opacity = '0';
-    }, fadetime * 1); 
+//     // First, fade out the text
+//     setTimeout(() => {
+//         loaderText.style.opacity = '0';
+//     }, fadetime * 1); 
 
-    // Next, fade out the image after text has faded
-    setTimeout(() => {
-        loaderImage.style.opacity = '0';
-    }, fadetime * 2); 
+//     // Next, fade out the image after text has faded
+//     setTimeout(() => {
+//         loaderImage.style.opacity = '0';
+//     }, fadetime * 2); 
 
-    // Finally, fade out the loader background after the image has faded
-    setTimeout(() => {
-        loader.classList.add("loader--hidden");
-    }, fadetime * 3); 
+//     // Finally, fade out the loader background after the image has faded
+//     setTimeout(() => {
+//         loader.classList.add("loader--hidden");
+//     }, fadetime * 3); 
 
-    // Listener to remove the loader after the background has faded
-    loader.addEventListener("transitionend", (event) => {
-        if (event.target === loader) {
-            document.body.removeChild(loader);
-        }
-    });
+//     // Listener to remove the loader after the background has faded
+//     loader.addEventListener("transitionend", (event) => {
+//         if (event.target === loader) {
+//             document.body.removeChild(loader);
+//         }
+//     });
+// });
+
+window.addEventListener("DOMContentLoaded", function() {
+    document.body.style.opacity = '1'; // Fade in the body
 });
 
 const observer = new IntersectionObserver((entries) => {
